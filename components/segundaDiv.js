a1f0105a6a0cb352f9814190deeaaf724e528dec
Matteus Isaque <matteus.isaque28@gmail.com>
2025-05-26 10:15:37 -0300

    Update segundaDiv.js

--- components/segundaDiv.js
+++ components/segundaDiv.js
@@ -7,19 +7,19 @@ export default function segundaDiv() {
             <div className={Style.BlocoItens}>
                 <div>
                     <h3>Treino Completo:</h3>
-                    <p>Irei fazer uma avaliação completa e definir uma rotina de exercícios que irão desde as partes inferiores até as superiores. Esse cronograma será descrito de acordo com seu perfil.</p>
+                    <p>Farei uma avaliação detalhada e definirei uma rotina de exercícios que abrange tanto os membros inferiores quanto os superiores. Esse cronograma será elaborado de acordo com o seu perfil.</p>
                 </div>
                 <div>
                     <h3>Suporte para tirar dúvidas:</h3>
-                    <p>Você terá acesso direto a mim para poder tirar suas dúvidas e receberá auxílio de um profissional</p>
+                    <p>Você terá acesso direto a mim para esclarecer suas dúvidas e contará com o acompanhamento de um profissional.</p>
                 </div>
                 <div>
                     <h3>Otimização de treino:</h3>
-                    <p>Tenha o treino modificado de acordo com a sua evolução, criando novas metas a serem alcançadas.</p>
+                    <p>Seu treino será ajustado conforme sua evolução, com novas metas sendo definidas ao longo do processo.</p>
                 </div>
                 <div>
                     <h3>Horários flexíveis:</h3>
-                    <p>Agende o melhor dia e hora para começar os seus treinamentos</p>
+                    <p>Agende o melhor dia e horário para iniciar seus treinamentos.</p>
                 </div>
             </div>
         </section>
